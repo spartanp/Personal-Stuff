bc638db9ee6cff3555f6760983569a216a5c4b4e
Prathap Natarajan <prathapnirmal@gmail.com>
2011-09-21 10:08:05 -0700

    add log4j dependency

--- dotcloud/springmvc/src/main/java/com/spartan/springmvc/FbController.java
+++ dotcloud/springmvc/src/main/java/com/spartan/springmvc/FbController.java
@@ -1,5 +1,6 @@
 package com.spartan.springmvc;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/fb")
 public class FbController {
+    private Logger logger = Logger.getLogger(this.getClass());
     
     @RequestMapping(method = RequestMethod.GET)
     public String hello() {
+        logger.info("facebook request came in");
+        return "hello";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String hellopost() {
+        logger.info("facebook request came in");
         return "hello";
     }
 
